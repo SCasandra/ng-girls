3b5f323627523deb88c3f35e14a0af999e78830a
CasandraS <casandra_sucala@yahoo.com>
2021-07-08 14:27:40 +0300

    refactor app component, add list-manager

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.scss']
+})
+export class ListManagerComponent implements OnInit {
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addItem(title: string){
+    this.todoList.push({title});
+  }
+
+}
